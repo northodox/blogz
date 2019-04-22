53956f308cdcaeb172e987b68122f0eb5b632f0d
Chris <northodontist@gmail.com>
2019-04-22 18:34:22 -0500

    started creating the User class in main.py

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
+    owner_id = db.Column(db.Integer)
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(60))
+    blogs = db.Column(db.Integer)
 
 @app.route('/', methods = ['POST','GET'])
 def index():
