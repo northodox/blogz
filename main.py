5aa9ca575ba1d257d492bfb09c6c8ff232cc11dd
Chris <northodontist@gmail.com>
2019-04-22 00:19:34 -0500

    made post.html page

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:badpassword@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -17,6 +17,6 @@ class Blog(db.Model):
         self.body = body
 
 @app.route('/')
-    def index():
-        return redirect('/blog')
+def index():
+    return redirect('/blog')
 
